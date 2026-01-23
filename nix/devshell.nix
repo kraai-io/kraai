25c68075c3789a6155cdbaf17326590b7a2071ac
ominit <86736586+ominit@users.noreply.github.com>
2026-01-23 12:58:11 -0500

    add template

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -21,6 +21,17 @@
 
           pkg-config
           openssl
+          at-spi2-atk
+          atkmm
+          cairo
+          gdk-pixbuf
+          glib
+          gtk3
+          harfbuzz
+          librsvg
+          libsoup_3
+          pango
+          webkitgtk_4_1
         ];
       };
     };
