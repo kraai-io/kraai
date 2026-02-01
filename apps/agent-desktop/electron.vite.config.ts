b25e355b52121cd2ad5f0363afaf27862a23e062
ominit <86736586+ominit@users.noreply.github.com>
2026-01-31 19:37:40 -0500

    setup tailwind and shadcn

--- apps/agent-desktop/electron.vite.config.ts
+++ apps/agent-desktop/electron.vite.config.ts
@@ -1,16 +1,42 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "electron-vite";
+import tailwindcss from "@tailwindcss/vite"
 import { resolve } from "path";
 
 export default defineConfig({
-	main: {},
-	preload: {},
+	main: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/preload/index.ts')
+        }
+      }
+    }
+  },
 	renderer: {
-		resolve: {
-			alias: {
-				"@renderer": resolve("src/renderer/src"),
-			},
-		},
-		plugins: [react()],
+	  resolve: {
+           alias: {
+                   "@renderer": resolve("src"),
+                   "@": resolve(".")
+           },
+   },
+	  root: '.',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'index.html')
+        }
+      }
+    },
+		plugins: [react(), tailwindcss()],
 	},
 });
--- apps/agent-desktop/src/App.tsx
+++ apps/agent-desktop/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App(): React.JSX.Element {
 	return (
 		<div>
-			<h1>Agent Demo</h1>
+			<h1 class="underline">Agent Demo</h1>
 			<p>Agent desktop application placeholder</p>
+			<Button>a</Button>
 		</div>
 	);
 }
